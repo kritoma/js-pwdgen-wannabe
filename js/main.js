f1d905c1ad3b365965f4fa6c1b7d066143619c3d
kritoma <toma.kristian97@gmail.com>
2022-04-27 16:51:13 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,4 @@
+const name = prompt("Qual'è il tuo nome?");
+const surname = prompt("Qual'è il tuo cognome?");
+const favoriteColor = prompt("Qual'è il tuo colore preferito?");
+document.getElementById("password").innerHTML = name + surname + favoriteColor + "21";
